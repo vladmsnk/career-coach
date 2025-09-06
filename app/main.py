3f9d02b42a75af6287081afb879aa36a72a57ce9
vymoiseenkov <vymoiseenkov@avito.ru>
2025-09-06 13:14:12 +0300

    login ui

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.routes.auth import router as auth_router
 from app.api.v1.routes.chat import router as chat_router
@@ -6,6 +7,15 @@ from app.api.v1.routes.chat import router as chat_router
 
 def create_app() -> FastAPI:
     application = FastAPI(title="Chat Service", version="0.1.0")
+
+    # CORS
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # Routers
     application.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
